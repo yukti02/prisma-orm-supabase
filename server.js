efd9ffc4f78d81e4cdd82ed7506cdd1edcf98652
Yukti Jain <yuktij02@gmail.com>
2023-09-13 18:33:44 +0530

    files added

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const server = express()
+
+server.use(express.json())
+server.listen(5432, () => console.log('Server is running'))
+
+// create a row
+server.post('/series', async (req, res) => {
+    const { name, description, release_year, rating, reviews } = req.body;
+
+    const series = await prisma.series.create({
+        data: {
+            name,
+            description,
+            release_year,
+            rating,
+            reviews,
+        },
+    });
+
+    res.status(201).json(series);
+});
+
+// get all the series
+server.get('/series', async (req, res) => {
+    const seriesList = await prisma.series.findMany();
+    res.status(200).json(seriesList);
+});
+
+// delete a row by id
+server.delete('/series/:id', async (req, res) => {
+    const seriesId = parseInt(req.params.id);
+
+    const isSeries = await prisma.series.findUnique({
+        where: { id: seriesId },
+    });
+
+    if (!isSeries) {
+        return res.status(404).json({ error: 'Series not found' });
+    }
+
+    await prisma.series.delete({
+        where: { id: seriesId },
+    });
+    res.status(204).send({ message: 'Series deleted' });
+});
+
+// update a row by id
+server.put('/series/:id', async (req, res) => {
+    const seriesId = parseInt(req.params.id);
+
+    const isSeries = await prisma.series.findUnique({
+        where: { id: seriesId },
+    });
+
+    if (!isSeries) {
+        return res.status(404).json({ error: 'Series not found' });
+    }
+
+    const series = await prisma.series.update({
+        where: { id: seriesId },
+        data: req.body,
+    });
+    res.status(200).json(series);
+});
+
+// get all season details from a given id
+server.get('/series/:id', async (req, res) => {
+    const seriesId = parseInt(req.params.id);
+
+    const isSeries = await prisma.series.findUnique({
+        where: { id: seriesId },
+        include: { seasons: true }
+    }
+    );
+
+    if (!isSeries) {
+        return res.status(404).json({ error: 'Series not found' });
+    }
+
+    res.status(200).json(isSeries);
+})
+
+
+
